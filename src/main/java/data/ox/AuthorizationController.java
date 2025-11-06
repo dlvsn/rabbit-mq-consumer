684146a46102ce9805ae18db6a0d27eed82d9fea
Denys Mazurenko <denys.mazurenko@data-ox.com>
2025-11-06 16:09:02 +0200

    Added auto-delete for inactive queues

--- src/main/java/data/ox/AuthorizationController.java
+++ src/main/java/data/ox/AuthorizationController.java
@@ -1,7 +1,10 @@
 package data.ox;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
--- src/main/java/data/ox/ClientInfrastructureService.java
+++ src/main/java/data/ox/ClientInfrastructureService.java
@@ -7,10 +7,16 @@ import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.stereotype.Service;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class ClientInfrastructureService {
+    private static final Map<String, Object> ARGUMENTS = Map
+            .of(
+                    "x-message-ttl", 30000,
+                    "x-expires", 60000
+            );
     private static final String QUEUE_NAME_TEMPLATE = "queue_%s";
     private final RabbitAdmin rabbitAdmin;
     private final DirectExchange directExchange;
@@ -22,7 +28,10 @@ public class ClientInfrastructureService {
         Queue queue = new Queue(queueName,
                 true,
                 false,
-                false);
+                false,
+                ARGUMENTS
+        );
+
         rabbitAdmin.declareQueue(queue);
 
         Binding binding = BindingBuilder
